8b4954fba35298b28d747f4016cee91a16a7b893
Connor Nauert <cnau06@gmail.com>
2026-02-06 16:11:13 -0600

    Add movement keybinds and mouse lock.

--- scripts/benson.gd
+++ scripts/benson.gd
@@ -7,28 +7,33 @@ extends CharacterBody3D
 var flying : bool = false
 var current_flight_time: float = 0
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 		flying = false
 	
 	# Handle flight.
 	if is_on_floor():
 		current_flight_time = 0
-	if Input.is_action_just_released("ui_accept"):
+	if Input.is_action_just_released("jump"):
 		flying = true
 	if Input.is_action_pressed("ui_accept") and !is_on_floor() and current_flight_time < flight_time and flying:
 		velocity.y = flight_velocity
 		current_flight_time += delta
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
