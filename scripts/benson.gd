47c2d1066198d9a9487b384f54661468669d650b
Connor Nauert <cnau06@gmail.com>
2026-02-06 19:48:52 -0600

    Fix mouse blocked by GUI in last commit

--- scripts/benson.gd
+++ scripts/benson.gd
@@ -26,6 +26,8 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
