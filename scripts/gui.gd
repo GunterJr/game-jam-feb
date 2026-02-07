8dddedb5cc755a59968edc599a42c9a22b44ae94
Connor Nauert <cnau06@gmail.com>
2026-02-07 01:33:06 -0600

    Add letter giving and text box for suitor bee. Note: GUI is now an autoload.

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -1,6 +1,23 @@
 extends Control
 
+@onready var flight_remaining: Label = $MarginContainer/VBoxContainer/FlightRemaining
+@onready var letter_status: Label = $MarginContainer/VBoxContainer/LetterStatus
+
+## Really, this should not be stored in a GUI singleton, it probably should be 
+## some other autoload for game state. Oh well.
+var has_letter : bool = false:
+	set(input):
+		letter_label(input)
+
 func update_flight(new: float):
 	new = snappedf(new, 0.1)
 	var out : String = "FLIGHT: " + str(new)
-	$MarginContainer/FlightRemaining.text = out
+	flight_remaining.text = out
+	
+func letter_label(letter : bool) -> void:
+	if letter:
+		letter_status.text = "Holding Letter!"
+		letter_status.add_theme_color_override("font_color", Color(0.0, 0.808, 0.0, 1.0))
+	else:
+		letter_status.text = "No Letter"
+		letter_status.add_theme_color_override("font_color", Color(1.0, 0.0, 0.0, 1.0))
